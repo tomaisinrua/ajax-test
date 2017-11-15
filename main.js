64aef669a9e378c99b987d8c671a38db6a823592
Tommy Walsh <tommywalsh1984@gmail.com>
2017-11-15 22:10:35 +0000

    working

--- main.js
+++ main.js
@@ -13,8 +13,35 @@ function getData(type, cb) {
     };
 }
 
+function getTableHearers(obj) {
+    var tableHeaders = [];
+    
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+    
+    return `<tr>${tableHeaders}</tr>`;
+}
+
 function writeToDocument(type) {
+    var tableRows = [];
+    var el = document.getElementById("data");
+    el.innerHTML = "";
     getData(type, function(data) {
-       document.getElementById("data").innerHTML = data; 
+        data = data.results;
+        var tableHeaders = getTableHearers(data[0]);
+        
+        data.forEach(function(item) {
+            var dataRow = [];
+            
+            Object.keys(item).forEach(function(key) {
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15);
+                dataRow.push(`<td>${truncatedData}</td>`); 
+            });
+            tableRows.push(`<tr>${dataRow}</tr>`);
+        });
+        
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
     });
 }
